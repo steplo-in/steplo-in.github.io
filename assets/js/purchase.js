477e2848744aef00a85c24271a48179d85011e27
steplo-in <97248228+steplo-in@users.noreply.github.com>
2022-01-07 10:12:46 +0530

    Add files via upload

---
+++ assets/js/purchase.js
@@ -0,0 +1,26 @@
+
+var courseID=document.currentScript.getAttribute('courseID');
+var courseName=document.currentScript.getAttribute('courseName');
+var coursePrice=Number(document.currentScript.getAttribute('coursePrice'));
+
+dataLayer.push({
+	'couseID': courseID,
+	'courseName':courseName,
+	'coursePrice':coursePrice,
+	'event': 'customView'
+  });
+
+var buttons = document.getElementsByClassName("Course");
+var gtmFunction = function() {
+	dataLayer.push({
+		'couseID': courseID,
+		'courseName':courseName,
+		'coursePrice':coursePrice,
+		'event': 'customPurchase'
+	  });
+	};
+for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', gtmFunction, false);
+}	
+
+
