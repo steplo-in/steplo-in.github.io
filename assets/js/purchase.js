ae177ff3588084aefe2265c951e1ecd6e1dadcf7
steplo-in <97248228+steplo-in@users.noreply.github.com>
2022-01-16 12:35:18 +0530

    updated ecom items array

--- assets/js/purchase.js
+++ assets/js/purchase.js
@@ -10,7 +10,8 @@ dataLayer.push({
 	'coursePrice':coursePrice,
 	'courseCurrency':'INR',
 	'courseItems':[{'id':courseID,'google_business_vertical':'retail'}],
-	'ecomItems':[{'item_id':courseID,"item_name":courseName}]
+	'ecomItems':[{'item_id':courseID,"item_name":courseName,'currency':'INR','price':coursePrice,'quantity':1}],
+
   });
 
 var buttons = document.getElementsByClassName("Course");
@@ -22,7 +23,7 @@ var gtmFunction = function() {
 		'coursePrice':coursePrice,
 		'courseCcurrency':'INR',
 		'courseItems':[{'id':courseID,'google_business_vertical':'retail'}],
-		'ecomItems':[{'item_id':courseID,"item_name":courseName}],
+		'ecomItems':[{'item_id':courseID,"item_name":courseName,'currency':'INR','price':coursePrice,'quantity':1}],
 		'purchaseTransactionId':"T_".concat((Math.random() + 1).toString(36).substring(7)).concat(Date.now()).concat((Math.random() + 1).toString(36).substring(7))
 	  });
 	};
